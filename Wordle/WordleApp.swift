537bf5c3d9abbe716cceaa7af3c668564225268f
tugskh <tugsbillegkh@gmail.com>
2023-06-09 09:41:56 -0400

    initial commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Tugs Khaliunbat on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
