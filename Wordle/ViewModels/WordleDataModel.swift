c7465e2bad98603c672e15464c5da9a7bd5b7afb
tugskh <tugsbillegkh@gmail.com>
2023-06-09 14:15:10 -0400

    game UI done

--- Wordle/ViewModels/WordleDataModel.swift
+++ Wordle/ViewModels/WordleDataModel.swift
@@ -11,10 +11,12 @@ class WordleDataModel: ObservableObject {
     
     @Published var guesses: [Guess] = []
     
+    var keyColors = [String: Color]()
+    
     init() {
         newGame()
     }
-    
+    // MARK: - Setup
     func newGame() {
         populateDefaults()
     }
@@ -24,5 +26,24 @@ class WordleDataModel: ObservableObject {
         for index in 0...5 {
             guesses.append(Guess(index: index))
         }
+        // reset keyboard colors
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for char in letters {
+            keyColors[String(char)] = .unused
+        }
+    }
+    
+    
+    // MARK: - Game Play
+    func addToCurrentWord(_ letter: String) {
+        
+    }
+    
+    func enterWord() {
+        
+    }
+    
+    func removeLetterFromCurrentWord() {
+        
     }
 }
